9306d57935ac213b9564817c01b5eb8c37a02ccd
ozelifaydin <57552372+ozelifaydin@users.noreply.github.com>
2019-11-10 14:50:05 -0600

    third

--- src/First.java
+++ src/First.java
@@ -2,6 +2,7 @@ public class First {
     //public static void main(String[] args) {
         void method1(){
             System.out.println("hi");
+            System.out.println("naber");
 
     }
     }
